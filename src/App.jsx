ff22b4412ce20f08c19bce5855b913e471029e06
annydrn <andrea.duran.cabana@gmail.com>
2022-11-02 21:49:26 -0300

    actualizacion de codigo para portafolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 import Cover from './components/Cover';
+import Navbar from './components/Navbar';
+import About from './components/About';
 
 function App() {
+const [scrollHeight, setscrollHeight] = useState(0);
 
+const handleScroll= () => {
+   const position = window.pageYOffset;
+   setscrollHeight(position);
+   }
+
+useEffect(() => {
+  window.addEventListener("scroll", handleScroll);
+}, [setscrollHeight])
+
+  
   return (
     <div className="App">
+     <Navbar isScrolling={scrollHeight}/>
      <Cover/>
-
+    <About/>
       
        
     </div>
---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "../styles/about.css"
+
+const About = () => {
+  return (
+    <div className='about-container'>About
+    <div className='about-desc'>
+        <h3>tell you somethine</h3>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque quo unde eius sequi dolore animi at doloribus rem suscipit rerum, veniam omnis veritatis eligendi ad, eveniet expedita totam id. Earum.</p>
+    </div>
+    <div className='about-img'>
+        <img  src="public/img/portafoliophoto.png" alt="" />
+    </div>
+    </div>
+  )
+}
+
+export default About
